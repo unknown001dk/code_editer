aaf801a90c311c4415ff7ffe352c241455e8fde5
Dineshkumar <117102544+unknown001dk@users.noreply.github.com>
2023-12-05 20:04:23 +0530

    code is updated

---
+++ coder.js
@@ -0,0 +1,10 @@
+function run(){
+  let htmlcode = document.getElementById("html-code");
+  let csscode = document.getElementById("css-code");
+  let jscode = document.getElementById("js-code");
+  let output = document.getElementById("output");
+
+  output.contentDocument.body.innerHTML = htmlcode.value + "<style>" + csscode.value + "</style>";
+  
+  output.contentWindow.eval(jscode.value);
+}
